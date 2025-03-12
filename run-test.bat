74c7d7bf36c0e6c5bd5567b6583afe168f188d34
abhi <abhishiek.331@gmail.com>
2025-03-11 22:02:29 -0400

    Deleting some unnecessary files

--- run-test.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd backend
-call mvnw.cmd spring-boot:run "-Dspring-boot.run.profiles=test" 
--- run-test.ps1
+++
@@ -1,2 +0,0 @@
-Set-Location -Path "backend"
-& "./mvnw.cmd" "spring-boot:run" "-Dspring-boot.run.profiles=test" 
